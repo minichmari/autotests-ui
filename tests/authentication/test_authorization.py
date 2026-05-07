4f314fa189d9f21195526c1469c7a51e7ff8a5f9
minichmari <porcelainmasha@gmail.com>
2026-05-07 08:42:31 +0300

    Практика работы с Page Factory

--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -1,5 +1,5 @@
 import pytest
-from pages.login_page import LoginPage
+from pages.authentication.login_page import LoginPage
 
 
 @pytest.mark.regression
--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -1,8 +1,8 @@
 import pytest
 
 
-from pages.courses_list_page import CoursesListPage
-from pages.create_course_page import CreateCoursePage
+from pages.courses.courses_list_page import CoursesListPage
+from pages.courses.create_course_page import CreateCoursePage
 
 
 @pytest.mark.regression
